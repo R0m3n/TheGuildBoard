e19f9a81808671f490e50ef63d38407c8a5661da
Owen Stevens <ows2023@yahoo.com>
2021-04-16 12:44:19 -0600

    Basic Models

---
+++ TheGuildBoard/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheGuildBoard.Models
+{
+    public class Message
+    {
+        public int SenderId { get; set; }
+        public int ReceiverProperty { get; set; }
+        public string Contents { get; set; }
+    }
+}
---
+++ TheGuildBoard/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheGuildBoard.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Contents { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
